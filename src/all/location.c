d3ba61211cc7be94c1bc49a37be0f0870df4bae6
dsmurrow <73198549+dsmurrow@users.noreply.github.com>
2020-10-21 13:43:52 -0400

    Replaced spaces with tabs and added comments
    Added comments for location_setfile() and location_setrank().

--- src/all/location.c
+++ src/all/location.c
@@ -11,7 +11,12 @@ uint_fast8_t location_getrank(const Location loc)
 }
 
 
-
+/**
+ * Sets the file (x-coordinate) of a location struct to the first four bits of val.
+ *
+ * @param ptr The pointer to the location struct that is being modified
+ * @param val The value the file is being changed to
+ */
 void location_setfile(Location *ptr, const uint_fast8_t val)
 {
 	*ptr &= ~LOCATION_FILE;
@@ -20,6 +25,12 @@ void location_setfile(Location *ptr, const uint_fast8_t val)
 	*ptr |= val << 4;
 }
 
+/**
+ * Sets the rank (y-coordinate of a location struct to the first four bits of val.
+ *
+ * @param ptr The pointer to the location struct being modified
+ * @param The value of the new rank
+ */
 void location_setrank(Location *ptr, const uint_fast8_t val)
 {
 	*ptr &= ~LOCATION_RANK;
@@ -28,10 +39,18 @@ void location_setrank(Location *ptr, const uint_fast8_t val)
 	*ptr |= val;
 }
 
-
+/**
+ * Evaluates if the given coordinates are equivalent to loc.
+ *
+ * @param loc The location struct that is being compared
+ * @param x The horizontal coordinate
+ * @param y The vertical coordinate
+ *
+ * @returns true if a location assigned the coordinates x and y is equal to loc
+ */
 bool location_equals_coords(const Location loc, const uint_fast8_t x, const uint_fast8_t y)
 {
-    Location temp;
+	Location temp;
 	location_assign(&temp, x, y);
 
 	return loc == temp;
@@ -47,15 +66,15 @@ bool location_equals_coords(const Location loc, const uint_fast8_t x, const uint
  */
 void location_to_coordinate_string(char *coord, Location loc)
 {
-    coord[0] = location_getfile(loc) + 96;
-    coord[1] = location_getrank(loc) + '0';
-    coord[2] = '\0';
+	coord[0] = location_getfile(loc) + 96;
+	coord[1] = location_getrank(loc) + '0';
+	coord[2] = '\0';
 }
 
 
 void location_assign(Location *loc, const uint_fast8_t x, const uint_fast8_t y)
 {
-    location_setfile(loc, x);
-    location_setrank(loc, y);
+	location_setfile(loc, x);
+	location_setrank(loc, y);
 }
 
