784a337a27cf7b2e06de077b2eb7f1d1df6817ea
dsmurrow <73198549+dsmurrow@users.noreply.github.com>
2020-10-21 00:29:52 -0400

    Removed unecessary occurences of const keyword

--- src/all/char.h
+++ src/all/char.h
@@ -1,17 +1,17 @@
-#ifndef STRINGS_H_INCLUDED
-#define STRINGS_H_INCLUDED
+#ifndef CHAR_H_INCLUDED
+#define CHAR_H_INCLUDED
 
 #include <inttypes.h>
 #include <stdbool.h>
 #include <stdlib.h>
 
-bool char_is_coord(const char);
-bool char_is_digit(const char);
-bool char_is_piece(const char);
-bool char_array_containts(const char*, const uintmax_t, const char);
+bool char_is_coord(char);
+bool char_is_digit(char);
+bool char_is_piece(char);
+bool char_array_containts(const char*, uintmax_t, char);
 
 uintmax_t string_getlen(const char*);
-uintmax_t string_add_char(char*, const char);
+uintmax_t string_add_char(char*, char);
 uintmax_t string_concatenate(char*, const char*);
 void string_remove(char*, const uintmax_t);
 void string_copy(char*, const char*);
@@ -21,9 +21,9 @@ void tokenize_move(char*);
 void string_tolower(char*);
 bool string_matches(const char*, const char*);
 bool string_matches_end(const char*);
-bool string_contains(const char*, const char);
-uintmax_t string_count_occurences_of_char(const char*, const char);
-uintmax_t string_split(char*, const uintmax_t, const char*, const char);
-uintmax_t string_split_malloc(char**, const char*, const char);
+bool string_contains(const char*, char);
+uintmax_t string_count_occurences_of_char(const char*, char);
+uintmax_t string_split(char*, uintmax_t, const char*, char);
+uintmax_t string_split_malloc(char**, const char*, char);
 
 #endif /* CHAR_H_INCLUDED */
