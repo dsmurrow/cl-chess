52c87e7bb019520d194d0d5ee9651d26a64556f8
dsmurrow <73198549+dsmurrow@users.noreply.github.com>
2020-10-23 01:43:55 -0400

    Replaced spaces with tabs

--- src/all/chess.h
+++ src/all/chess.h
@@ -6,10 +6,10 @@
 #include "piece.h"
 
 #ifdef __WIN32
-    #include <windows.h>
-    #define RESETCOLOR makeColor(WHITE, BLACK)
+	#include <windows.h>
+	#define RESETCOLOR makeColor(WHITE, BLACK)
 #else
-    #define RESETCOLOR printf("\033[0m")
+	#define RESETCOLOR printf("\033[0m")
 #endif
 
 typedef struct player Player;
@@ -19,11 +19,11 @@ typedef struct game Game;
 
 struct game
 {
-    MoveList Moves;
-    Piece *White[PIECES_PER_SIDE];
-    Piece *Black[PIECES_PER_SIDE];
-    Location *pieceLocations[2 * PIECES_PER_SIDE]; /* when a piece is captured set it's location to NULL, and when */
-};                                                 /* checking element in this, first check if the element is NULL */
+	MoveList Moves;
+	Piece *White[PIECES_PER_SIDE];
+	Piece *Black[PIECES_PER_SIDE];
+	Location *pieceLocations[2 * PIECES_PER_SIDE]; 	/* when a piece is captured set it's location to NULL, and when */
+};													/* checking element in this, first check if the element is NULL */
 
 
 Game *init_game();
