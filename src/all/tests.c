ba3347a0a5abde00414f25337d54f89776fc36b7
dsmurrow <73198549+dsmurrow@users.noreply.github.com>
2020-10-24 15:09:12 -0400

    Replaced some spaces with tabs, removed test_pawns_illegal()
    test_pawns_illegal() was made redundant when I changed the input validation in locations.c

--- src/all/tests.c
+++ src/all/tests.c
@@ -3,76 +3,75 @@
 
 void test_valid_macros()
 {
-    uint_fast8_t i, j;
-
-    uint_fast8_t is_validMacros[5] = {VALID_BROADCASTCALL, VALID_IGNORECOLOR, VALID_KINGNOLIMIT, VALID_PAWNATKONLY, VALID_SELFCHECK};
-    for(i = 0; i < 5; i++)
-    {
-        for(j = i + 1; j < 5; j++)
-        {
-            assert(!(is_validMacros[i] & is_validMacros[j]));
-        }
-    }
+	uint_fast8_t i, j;
+	uint_fast8_t is_validMacros[5] = {VALID_BROADCASTCALL, VALID_IGNORECOLOR, VALID_KINGNOLIMIT, VALID_PAWNATKONLY, VALID_SELFCHECK};
+	
+	for(i = 0; i < 5; i++)
+	{
+		for(j = i + 1; j < 5; j++)
+		{
+			assert(!(is_validMacros[i] & is_validMacros[j]));
+		}
+	}
 }
 
 void test_PGN_macros()
 {
-    assert((char)(PGN_PROMOTION_QUEEN & PGN_PROMOTIONMASK) == 'Q');
-    assert((char)(PGN_PROMOTION_BISHOP & PGN_PROMOTIONMASK) == 'B');
-    assert((char)(PGN_PROMOTION_KNIGHT & PGN_PROMOTIONMASK) == 'N');
-    assert((char)(PGN_PROMOTION_ROOK & PGN_PROMOTIONMASK) == 'R');
-
-
+	assert((char)(PGN_PROMOTION_QUEEN & PGN_PROMOTIONMASK) == 'Q');
+	assert((char)(PGN_PROMOTION_BISHOP & PGN_PROMOTIONMASK) == 'B');
+	assert((char)(PGN_PROMOTION_KNIGHT & PGN_PROMOTIONMASK) == 'N');
+	assert((char)(PGN_PROMOTION_ROOK & PGN_PROMOTIONMASK) == 'R');
 }
 
 void test_piece_macros()
 {
-    uint_fast8_t i, j, piecechecks[5];
-    assert((PIECE_BISHOP | PIECE_ROOK) == PIECE_QUEEN);
+	uint_fast8_t i, j, piecechecks[5];
+	assert((PIECE_BISHOP | PIECE_ROOK) == PIECE_QUEEN);
 
-    piecechecks[0] = PIECE_PAWN;
-    piecechecks[1] = PIECE_ROOK;
-    piecechecks[2] = PIECE_KNIGHT;
-    piecechecks[3] = PIECE_BISHOP;
-    piecechecks[4] = PIECE_KING;
+	piecechecks[0] = PIECE_PAWN;
+	piecechecks[1] = PIECE_ROOK;
+	piecechecks[2] = PIECE_KNIGHT;
+	piecechecks[3] = PIECE_BISHOP;
+	piecechecks[4] = PIECE_KING;
 
-    for(i = 0; i < 5; i++)
-    {
-        for(j = i + 1; j < 5; j++)
-        {
-            assert(!(piecechecks[i] & piecechecks[j]));
-        }
-    }
+	for(i = 0; i < 5; i++)
+	{
+		for(j = i + 1; j < 5; j++)
+		{
+			assert(!(piecechecks[i] & piecechecks[j]));
+		}
+	}
 }
 
 void test_on_board_macro()
 {
-    int_fast8_t i, j;
+	int_fast8_t i, j;
 
-    i = INT_FAST8_MIN;
-    while(i > INT_FAST8_MIN)
-    {
-        j = INT_FAST8_MIN;
-        while(j > INT_FAST8_MIN)
-        {
-            if(i > 0 && i < 9 && j > 0 && j < 9)
-                assert(IS_ON_BOARD(i,j));
-            else
-                assert(!(IS_ON_BOARD(i, j)));
-            j++;
-        };
-        i++;
-    };
+	i = INT_FAST8_MIN;
+	while(i > INT_FAST8_MIN)
+	{
+		j = INT_FAST8_MIN;
+		while(j > INT_FAST8_MIN)
+		{
+			if(i > 0 && i < 9 && j > 0 && j < 9)
+				assert(IS_ON_BOARD(i,j));
+			else
+				assert(!(IS_ON_BOARD(i, j)));
+			j++;
+		};
+		
+		i++;
+	};
 }
 
 
 void test_macros()
 {
-    assert(TEAM_WHITE != TEAM_BLACK);
+	assert(TEAM_WHITE != TEAM_BLACK);
 
-    test_on_board_macro();
-    test_piece_macros();
-    test_valid_macros();
+	test_on_board_macro();
+	test_piece_macros();
+	test_valid_macros();
 }
 
 
@@ -177,7 +176,7 @@ void test_castling()
             capture(board, WhitePieces[i]);
 			capture(board, BlackPieces[i]);
     	}
-	}
+    }
 
     location_assign(&kingside, 7, 1);
     assert(is_valid_move(board, WhitePieces[I_KING], kingside, 0));
@@ -200,97 +199,54 @@ void test_castling()
 
 void test_pawn_forward()
 {
-    Game *board = init_game();
+	Game *board = init_game();
 
-    Piece **WhitePieces = board->White;
-    Piece **BlackPieces = board->Black;
+	Piece **WhitePieces = board->White;
+	Piece **BlackPieces = board->Black;
 
-    Location temp;
-    uint_fast8_t i;
-    for(i = I_PAWN1; i <= I_PAWN8; i++)
-    {
- 	Piece *w, *b;
+	Location temp;
+	uint_fast8_t i;
+	for(i = I_PAWN1; i <= I_PAWN8; i++)
+	{
+		Piece *w, *b;
 
-        w = WhitePieces[i];
+		w = WhitePieces[i];
 
-	location_assign(&temp, location_getfile(w->currentLocation), 3);
-        assert(is_valid_move(board, w, temp, 0));
+		location_assign(&temp, location_getfile(w->currentLocation), 3);
+		assert(is_valid_move(board, w, temp, 0));
 
-        location_setrank(&temp, location_getrank(temp) + 1);
-        assert(is_valid_move(board, w, temp, 0));
+		location_setrank(&temp, location_getrank(temp) + 1);
+		assert(is_valid_move(board, w, temp, 0));
 
 
-        b = BlackPieces[i];
+		b = BlackPieces[i];
 
-        location_setrank(&temp, 6);
-        assert(is_valid_move(board, b, temp, 0));
+		location_setrank(&temp, 6);
+		assert(is_valid_move(board, b, temp, 0));
 
-        location_setrank(&temp, location_getrank(temp) - 1);
-        assert(is_valid_move(board, b, temp, 0));
-    }
+		location_setrank(&temp, location_getrank(temp) - 1);
+		assert(is_valid_move(board, b, temp, 0));
+	}
 
-    free_game(board);
+	free_game(board);
 }
 
 void test_pawn_capture()
 {
-    Piece *w, *b;
-    Game *board = init_game();
+	Piece *w, *b;
+	Game *board = init_game();
 
-    w = board->White[I_PAWN5];
+	w = board->White[I_PAWN5];
 	location_setrank(&(w->currentLocation), 4);
 
-    b = board->Black[I_PAWN4];
-    location_setrank(&(b->currentLocation), 5);
+	b = board->Black[I_PAWN4];
+	location_setrank(&(b->currentLocation), 5);
 
-    assert(is_valid_move(board, w, b->currentLocation, 0));
-    assert(is_valid_move(board, b, w->currentLocation, 0));
-
-    free_game(board);
+	assert(is_valid_move(board, w, b->currentLocation, 0));
+	assert(is_valid_move(board, b, w->currentLocation, 0));
+	
+	free_game(board);
 }
-
-void test_pawn_illegal()
-{
-    Game *board;
-    Piece **W, **B;
-    Location temp;
-    uint_fast8_t i, j;
-
-    board = init_game();
-
-    W = board->White;
-    B = board->Black;
-
-    for(i = I_PAWN1; i <= I_PAWN8; i++)
-    {
-        for(j = 1; j <= 8; j++)
-        {
-	    location_assign(&temp, j, 0);
-            assert(!is_valid_move(board, W[i], temp, 0));
-
-            location_setrank(&temp, 8);
-            assert(!is_valid_move(board, B[i], temp, 0));
-        }
-
-
-  	location_assign(&temp, location_getfile(W[i]->currentLocation) - 1, 3);
-        assert(!is_valid_move(board, W[i], temp, 0));
-
-        location_setfile(&temp, location_getfile(temp) + 2);
-        assert(!is_valid_move(board, W[i], temp, 0));
-
-		location_assign(&temp, location_getfile(B[i]->currentLocation) - 1, 6);
-        assert(!is_valid_move(board, B[i], temp, 0));
-
-        location_setfile(&temp, location_getfile(temp) + 2);
-        assert(!is_valid_move(board, B[i], temp, 0));
-    }
-
-    free_game(board);
-}
-
-
-
 
 void test_decipher()
 {
@@ -341,59 +297,66 @@ void test_decipher()
 
 void test_string_remove()
 {
-    char egg[17] = "dog";
-    char de[14] = "dog";
-    string_remove(egg, 2);
-    assert(egg[2] == '\0');
+	char egg[17] = "dog";
+	char de[14] = "dog";
+	
+	string_remove(egg, 2);
+	assert(egg[2] == '\0');
 
-    string_remove(de, 0);
-    assert(de[0] == 'o' && de[1] == 'g');
+	string_remove(de, 0);
+	assert(de[0] == 'o' && de[1] == 'g');
 }
 
 void test_string_copy()
 {
-    const char *ye = "ab";
-    char n[5];
-    string_copy(n, ye);
-    assert(n[0] == 'a');
-    assert(n[1] == 'b');
-    assert(n[2] == '\0');
+	const char *ye = "ab";
+	char n[5];
+	
+	string_copy(n, ye);
+	
+	assert(n[0] == 'a');
+	assert(n[1] == 'b');
+	assert(n[2] == '\0');
 }
 
 void test_string_count_occurences()
 {
-    const char *pi = "aaabbcccc";
-    assert(string_count_occurences_of_char(pi, 'a') == 3);
-    assert(string_count_occurences_of_char(pi, 'b') == 2);
-    assert(string_count_occurences_of_char(pi, 'c') == 4);
+	const char *pi = "aaabbcccc";
+	
+	assert(string_count_occurences_of_char(pi, 'a') == 3);
+	assert(string_count_occurences_of_char(pi, 'b') == 2);
+	assert(string_count_occurences_of_char(pi, 'c') == 4);
 }
 
 void test_string_cat()
 {
-    char dog[11] = "dog";
-    char *cat = "cat";
-    string_concatenate(dog, cat);
+	char dog[11] = "dog";
+	char *cat = "cat";
+	
+	string_concatenate(dog, cat);
 
-    assert(string_matches(dog, "dogcat"));
+	assert(string_matches(dog, "dogcat"));
 }
 
 void test_string_split()
 {
-    char empty[15][15];
-    const char *egg = "true dat homie";
-    uintmax_t splits = string_split(&empty[0][0], 15, egg, ' ');
-    assert(splits == 3);
-    assert(splits == string_count_occurences_of_char(egg, ' ') + 1);
-    assert(string_matches(empty[0], "true"));
-    assert(string_matches(empty[1], "dat"));
-    assert(string_matches(empty[2], "homie"));
+	char empty[3][7];
+	
+	const char *egg = "green eggs ham";
+	uintmax_t splits = string_split(&empty[0][0], 15, egg, ' ');
+	
+	assert(splits == 3);
+	assert(splits == string_count_occurences_of_char(egg, ' ') + 1);
+	assert(string_matches(empty[0], "green"));
+	assert(string_matches(empty[1], "eggs"));
+	assert(string_matches(empty[2], "ham"));
 }
 
 void test_tokenize()
 {
-    char a[8] = "bxc3#";
-    tokenize_move(a);
-    assert(a[0] == 'b' && a[1] == 'c' && a[2] == '3' && a[3] == '\0');
+	char a[8] = "bxc3#";
+	tokenize_move(a);
+	assert(a[0] == 'b' && a[1] == 'c' && a[2] == '3' && a[3] == '\0');
 }
 
 void test_move()
@@ -533,33 +496,31 @@ void test_move()
 
 void test_pawns()
 {
-    test_pawn_capture();
-    test_pawn_forward();
-    test_pawn_illegal();
+	test_pawn_capture();
+	test_pawn_forward();
 }
 
 void test_pieces()
 {
-    test_piece_placement();
-    test_castling();
-
-    test_pawns();
+	test_piece_placement();
+	test_castling();
+	test_pawns();
 }
 
 void test_functions()
 {
-    test_decipher();
-    test_move();
-    test_string_remove();
-    test_string_count_occurences();
-    test_string_split();
-    test_string_cat();
-    test_tokenize();
+	test_decipher();
+	test_move();
+	test_string_remove();
+	test_string_count_occurences();
+	test_string_split();
+	test_string_cat();
+	test_tokenize();
 }
 
 void testall()
 {
-    test_macros();
-    test_pieces();
-    test_functions();
+	test_macros();
+	test_pieces();
+	test_functions();
 }
